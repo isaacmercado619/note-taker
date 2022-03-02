eb530721d94318faba0de08d191dd62319ea033c
Isaac <mercado.isaac619@gmail.com>
2022-03-01 21:56:15 -0800

    hmm

--- server.js
+++ server.js
@@ -3,76 +3,68 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const util = require('util');
-const { json } = require('express/lib/response');
-
-//Handling Asynchronous Processes
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
+const notes = require('./Develop/db/db.json');
+const {v4: uuidv4} = require('uuid');
+const { response } = require('express');
 
 //Setting Up Server
 const app = express();
-const PORT = process.env.Port || 3000;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 // Static Middleware
-app.use(express.static("./develope/public"));
+app.use(express.static('Develop/public'));
 
 // API Route | "GET" request
 app.get("/api/notes", (req, res) => {
-    readFileAsync("./develop/db/db.json", "utf-8").then((data) => {
-        notes = [].concat(JSON.parse(data))
-        res.json(notes);
-    })
+    JSON.stringify(res.sendFile(path.join(__dirname, 'Develop/db/db.json')))
 });
-
-// API Route | "POST" request
-app.post("/api/notes", (req, res) => {
-    const note = req.body;
-    readFileAsync("./develop/db/db.json", "utf-8").then((data) => {
-        const notes = [].concat(JSON.parse(data));
-        notes.id = notes.length + 1
-        notes.push(note);
-        return notes
-    }).then((notes) => {
-        writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
-        res.join(note);
-    })
-});
-
-// API Route | "DELETE" request
-app.delete("/api/notes/:id", (req, res) => {
-    const idToDelete = parseInt(req.params.id);
-    readFileAsync("./develop/db/db.json", "utf-8").then((data) => {
-        const notes = [].concat(JSON.parse(data));
-        const newNotesData = []
-        for (let i = 0; i < notes.length; i++) {
-            if (idToDelete !== notes[i].id) {
-                newNotesData.push(notes[i])
-            }
-        }
-        return newNotesData
-    }).then((notes) => {
-        writeFileAsync("./develop/db/db.json", JSON.stringify(notes))
-        res.send('Saved Success!')
-    })
-})
 
 //HTML Routes
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./develop/public/notes.html"))
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"))
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./develop/public/index.html"))
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./develop/public/index.html"))
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"))
 });
+
+app.post('/api/notes', (req, res) => {
+    const createNote = req.body;
+    createNote.id = uuidv4();
+
+    let noteInfo = JSON.parse(fs.readFileSync('Develope/db/db.json', 'utf8'))
+
+    noteInfo.push(createNote)
+    fs.writeFileSync('Develope/db/db.json', JSON.stringify(noteInfo))
+    res.json(noteInfo)
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    let noteInfo = JSON.parse(fs.readFileSync('Develop/db/db.json', 'utf8'))
+    let noteId = req.params.id;
+
+    const noteIndex = noteInfo.map(note => note.id).indexOf(noteId)
+    console.log(noteIndex);
+    if (noteIndex >= 0) {
+        noteInfo.splice(noteIndex, 1)
+        console.log(noteInfo)
+        fs.writeFileSync('Develop/db/db.json', JSON.stringify(noteInfo))
+        res.json(noteInfo)
+    }else {
+        res.status(404).send({
+            message: 'Note not found!'
+        })
+    }
+})
 
 // Listening
 app.listen(PORT, () => {
-    console.log("App listening on PORT" + PORT)
+    console.log(`App listening at http://localhost:${PORT}`)
 });
